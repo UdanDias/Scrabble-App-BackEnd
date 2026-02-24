eff47ee86697331806a5f1192e848f960abff3f0
UdanDias <udandias892@gmail.com>
2026-02-24 15:52:39 +0530

    Created UserEntity

---
+++ src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/entity/security/UserEntity.java
@@ -0,0 +1,40 @@
+package lk.kelaniya.uok.scrabble.scrabbleapp.entity.security;
+
+import jakarta.persistence.*;
+import lk.kelaniya.uok.scrabble.scrabbleapp.dto.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class UserEntity implements Serializable, UserDetails {
+    @Id
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("Role_"+role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
