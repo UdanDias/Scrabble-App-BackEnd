5bbc639066153855733b2e729e44f64a4eea67ce
UdanDias <udandias892@gmail.com>
2026-02-24 13:26:19 +0530

    Created basic auth security filterchain class

--- src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/security/webSecurityConfig.java
+++ src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/security/webSecurityConfig.java
@@ -1,16 +1,41 @@
 package lk.kelaniya.uok.scrabble.scrabbleapp.security;
 
+import lk.kelaniya.uok.scrabble.scrabbleapp.config.CORSConfig;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class webSecurityConfig {
+    private final CORSConfig corsConfig;
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
 
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("cmjd")
+                .password("1234")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
